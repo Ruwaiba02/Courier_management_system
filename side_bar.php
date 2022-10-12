6d0debe097a5d9ef689ea2e4252836cfe8af9edc
Ruwaiba Jawaria <ruwaiba02@gmail.com>
2022-10-12 18:32:29 +0500

    initial commit

---
+++ side_bar.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<title>Management Admin</title>
+<link rel="icon" href="img/mini_logo.png" type="image/png">
+
+<link rel="stylesheet" href="css/bootstrap1.min.css" />
+
+<link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+
+<link rel="stylesheet" href="vendors/niceselect/css/nice-select.css" />
+
+<link rel="stylesheet" href="vendors/owl_carousel/css/owl.carousel.css" />
+
+<link rel="stylesheet" href="vendors/gijgo/gijgo.min.css" />
+
+<link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
+<link rel="stylesheet" href="vendors/tagsinput/tagsinput.css" />
+
+<link rel="stylesheet" href="vendors/datepicker/date-picker.css" />
+<link rel="stylesheet" href="vendors/vectormap-home/vectormap-2.0.2.css" />
+
+<link rel="stylesheet" href="vendors/scroll/scrollable.css" />
+
+<link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
+<link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
+<link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+
+<link rel="stylesheet" href="vendors/text_editor/summernote-bs4.css" />
+
+<link rel="stylesheet" href="vendors/morris/morris.css">
+
+<link rel="stylesheet" href="vendors/material_icon/material-icons.css" />
+
+<link rel="stylesheet" href="css/metisMenu.css">
+
+<link rel="stylesheet" href="css/style1.css" />
+<link rel="stylesheet" href="css/colors/default.css" id="colorSkinCSS">
+</head>
+
+<nav class="sidebar">
+    <div class="logo d-flex justify-content-between">
+    <a class="large_logo" href="index.php"><img src="img/logo.png" alt=""></a>
+    <a class="small_logo" href="index.php"><img src="img/mini_logo.png" alt=""></a>
+    <div class="sidebar_close_icon d-lg-none">
+    <i class="ti-close"></i>
+    </div>
+    </div>
+    <ul id="sidebar_menu">
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/dashboard.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>User Management </span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="index_2.php">Default</a></li>
+    <li><a href="index_3.php">Dark Sidebar</a></li>
+    <li><a href="index.php">Light Sidebar</a></li>
+    </ul>
+    </li>
+    
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/3.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Pages</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="login.php">Login</a></li>
+    <li><a href="resister.php">Register</a></li>
+    <li><a href="error_400.php">Error 404</a></li>
+    <li><a href="error_500.php">Error 500</a></li>
+    <li><a href="forgot_pass.php">Forgot Password</a></li>
+    <li><a href="gallery.php">Gallery</a></li>
+    </ul>
+    </li>
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/4.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Admins</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="admin_list.php">Admin List</a></li>
+    <li><a href="add_new_admin.php">Add New Admin</a></li>
+    </ul>
+    </li>
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/11.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Role & Permissions</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="module_setting.php">Module Setting</a></li>
+    <li><a href="role_permissions.php">Role & Permissions</a></li>
+    </ul>
+    </li>
+    
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/5.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Users</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="user_list.php">Users List</a></li>
+    <li><a href="add_new_user.php">Add New User</a></li>
+    </ul>
+    </li>
+    
+    <li class="">
+    <a href="invoice.php" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/7.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Invoice</span>
+    </div>
+    </a>
+    </li>
+    
+    
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/11.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Themes</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="dark_sidebar.php">Dark Sidebar</a></li>
+    <li><a href="light_sidebar.php">light Sidebar</a></li>
+    </ul>
+    </li>
+    
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/13.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Products</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="Products.php">Products</a></li>
+    <li><a href="Product_Details.php">Product Details</a></li>
+    <li><a href="Cart.php">Cart</a></li>
+    <li><a href="Checkout.php">Checkout</a></li>
+    </ul>
+    </li>
+    
+    <li class="">
+    <a class="has-arrow" href="#" aria-expanded="false">
+    <div class="nav_icon_small">
+    <img src="img/menu-icon/21.svg" alt="">
+    </div>
+    <div class="nav_title">
+    <span>Widgets</span>
+    </div>
+    </a>
+    <ul>
+    <li><a href="chart_box_1.php">Chart Boxes 1</a></li>
+    <li><a href="profilebox.php">Profile Box</a></li>
+    </ul>
+    </li>
+    
+    </ul>
+    </nav>
+
+
+    <script src="js/jquery1-3.4.1.min.js"></script>
+
+<script src="js/popper1.min.js"></script>
+
+<script src="js/bootstrap1.min.js"></script>
+
+<script src="js/metisMenu.js"></script>
+
+<script src="vendors/count_up/jquery.waypoints.min.js"></script>
+
+<script src="vendors/chartlist/Chart.min.js"></script>
+
+<script src="vendors/count_up/jquery.counterup.min.js"></script>
+
+<script src="vendors/niceselect/js/jquery.nice-select.min.js"></script>
+
+<script src="vendors/owl_carousel/js/owl.carousel.min.js"></script>
+
+<script src="vendors/datatable/js/jquery.dataTables.min.js"></script>
+<script src="vendors/datatable/js/dataTables.responsive.min.js"></script>
+<script src="vendors/datatable/js/dataTables.buttons.min.js"></script>
+<script src="vendors/datatable/js/buttons.flash.min.js"></script>
+<script src="vendors/datatable/js/jszip.min.js"></script>
+<script src="vendors/datatable/js/pdfmake.min.js"></script>
+<script src="vendors/datatable/js/vfs_fonts.js"></script>
+<script src="vendors/datatable/js/buttons.php5.min.js"></script>
+<script src="vendors/datatable/js/buttons.print.min.js"></script>
+
+<script src="vendors/datepicker/datepicker.js"></script>
+<script src="vendors/datepicker/datepicker.en.js"></script>
+<script src="vendors/datepicker/datepicker.custom.js"></script>
+<script src="js/chart.min.js"></script>
+<script src="vendors/chartjs/roundedBar.min.js"></script>
+
+<script src="vendors/progressbar/jquery.barfiller.js"></script>
+
+<script src="vendors/tagsinput/tagsinput.js"></script>
+
+<script src="vendors/text_editor/summernote-bs4.js"></script>
+<script src="vendors/am_chart/amcharts.js"></script>
+
+<script src="vendors/scroll/perfect-scrollbar.min.js"></script>
+<script src="vendors/scroll/scrollable-custom.js"></script>
+
+<script src="vendors/vectormap-home/vectormap-2.0.2.min.js"></script>
+<script src="vendors/vectormap-home/vectormap-world-mill-en.js"></script>
+
+<script src="vendors/apex_chart/apex-chart2.js"></script>
+<script src="vendors/apex_chart/apex_dashboard.js"></script>
+
+<script src="vendors/chart_am/core.js"></script>
+<script src="vendors/chart_am/charts.js"></script>
+<script src="vendors/chart_am/animated.js"></script>
+<script src="vendors/chart_am/kelly.js"></script>
+<script src="vendors/chart_am/chart-custom.js"></script>
+
+<script src="js/dashboard_init.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
