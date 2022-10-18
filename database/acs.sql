0a65b9cf457f81dee06f54d8524121301a68cc3c
Ruwaiba Jawaria <ruwaiba02@gmail.com>
2022-10-18 20:01:02 +0500

    new db

--- database/acs.sql
+++ database/acs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2022 at 07:51 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
+-- Generation Time: Oct 18, 2022 at 04:58 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,43 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `Address` varchar(100) NOT NULL,
-  `password` int(11) NOT NULL,
-  `role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `agents`
---
-
-CREATE TABLE `agents` (
-  `agent_id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `phone` varchar(15) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `Address` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `role` int(11) NOT NULL,
-  `branch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `branches`
 --
 
@@ -72,6 +35,14 @@ CREATE TABLE `branches` (
   `phone` varchar(15) NOT NULL,
   `email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`branch_id`, `branch_name`, `branch_city`, `branch_address`, `phone`, `email`) VALUES
+(1, 'Aptech', 'Karachi', 'North nazimabad', '123456789', 'abc@gmail.com'),
+(2, 'Aptechnn', 'Lahore', 'North Nazimabad', '123456789007', 'aptech@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -89,21 +60,44 @@ CREATE TABLE `parcels` (
   `delivery_date` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  `branch` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `username`, `phone`, `email`, `address`, `password`, `role`, `branch`) VALUES
+(1, 'Ruwaiba', 'Jawaria', 'ruwaiba', 1234567890, 'ruwaiba@gmail.com', 'North nazimabad', '12345', 1, 1),
+(2, 'Aimon', 'Jawed', 'aimon', 1234567890, 'aimon@gmail.com', 'North nazimabad', 'aimon123', 0, 2),
+(3, 'Zoha', 'Baig', 'zoha', 1234567890, 'zoha@gmail.com', 'North nazimabad', 'zoha123', 1, 1),
+(4, 'Muhammad', 'Bilal', 'bilal', 1234567890, 'bilal@gmail.com', 'North nazimabad', 'bilal123', 0, 2);
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `branches`
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `agents`
---
-ALTER TABLE `agents`
-  ADD PRIMARY KEY (`agent_id`);
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`branch_id`);
 
 --
 -- Indexes for table `parcels`
@@ -112,26 +106,43 @@ ALTER TABLE `parcels`
   ADD PRIMARY KEY (`Parcel_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `branch` (`branch`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `branches`
 --
-ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `agents`
---
-ALTER TABLE `agents`
-  MODIFY `agent_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `branches`
+  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `parcels`
 --
 ALTER TABLE `parcels`
   MODIFY `Parcel_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`branch`) REFERENCES `branches` (`branch_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
